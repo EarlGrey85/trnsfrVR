5a3761828772af0abc50f4bf8b64eaf8ba913dc2
AndreyKh <and.khorkov@gmail.com>
2020-09-20 23:07:28 -0700

    folders for ScriptableObjectsInstallers

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -8,8 +8,8 @@ namespace Simulation
     private readonly Transform _camRigTransform;
     private readonly Transform _camTransform;
     private readonly Transform _desiredPoint;
-    private Vector3 velocity;
-    private Vector3 vel;
+    private Vector3 moveVelocity;
+    private Vector3 rotationVelocity;
     
     public CameraController(Installables installables, SimulationFacade.Installables simInstallables)
     {
@@ -21,11 +21,11 @@ namespace Simulation
     void ITickable.Tick()
     {
       var position = _camRigTransform.position;
-      position = Vector3.SmoothDamp(position, _desiredPoint.position, ref velocity, 0.2f);
+      position = Vector3.SmoothDamp(position, _desiredPoint.position, ref moveVelocity, 0.2f);
       _camRigTransform.position = position;
 
       var forward = _camRigTransform.forward;
-      forward = Vector3.SmoothDamp(forward, _desiredPoint.forward, ref vel, 0.2f);
+      forward = Vector3.SmoothDamp(forward, _desiredPoint.forward, ref rotationVelocity, 0.2f);
       _camRigTransform.forward = forward;
     }
 
